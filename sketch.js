ddee7f3bb3e14492728d6d1ba59d4381cd971627
NamitaCODER <103303367+NamitaCODER@users.noreply.github.com>
2022-05-14 15:05:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,16 @@
 var box
 function setup() {
   createCanvas(400,400);
-  box=createSprite(200,200,20,10);
+box = createSprite(200,200,20,20)
 }
 
 function draw() 
 {
-  background(30);
-  if (keyDown("right") ){
-    box.position.x=box.position.x+2
+  background("yellow");
 
-  }
-  if (keyDown("left")){
-    box.position.x=box.position.x-2
-  }
-  if(keyDown("up")){
-    box.position.y=box.position.y-2
-  }
-  if (keyDown("down")){
-    box.position.y=box.position.y+2
-  }
-
+ if (keyDown("right")){
+   background("red")
+ }
   drawSprites();
 }
 
